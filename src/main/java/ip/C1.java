385edcb43a8a7ada74ee0466b706eab6fe678ca8
irek <irekpietek@o2.pl>
2017-09-10 21:25:45 +0200

    a5

---
+++ src/main/java/ip/C1.java
@@ -0,0 +1,11 @@
+package ip;
+
+/**
+ * Created by irek on 10.09.17.
+ */
+public class C1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!");
+    }
+}
---
+++ src/test/java/ip/TestC1.java
@@ -0,0 +1,17 @@
+package ip;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by irek on 10.09.17.
+ */
+public class TestC1 {
+
+    @Test
+    public void test() {
+        System.out.println("SDF");
+        Assert.assertEquals(1L, 11L);
+    }
+
+}
